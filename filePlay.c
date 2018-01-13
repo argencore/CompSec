65f08bbb81dea338a807ab027ed7fbe2bfa5cf35
Anthony Comstock <a32@pdx.edu>
2018-01-12 17:17:16 -0800

    filePlay filled in

---
+++ filePlay.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int filePlay(FILE fp)
+{
+    char sp[107];
+    int count = 0;
+    while(sp = getline(sp,107,fp) != NULL)
+    {
+        printf("%s\n",sp);
+        count++;
+    }
+    return count;
+}
+
+int main(int argsv, char **args)
+{
+    if(argsv > 0)
+    {
+        filePlay(args[1]);
+    }
+    return 0;
+}
+
